5988cfa5cf2cc7fa7db19c95dcb68ce96376fe84
Patiphan Phengpao <misterpatipan123@gmail.com>
2020-04-17 14:06:03 +0700

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,5 @@
+CREATE TABLE student (
+    id int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    student_name varchar(50) NOT NULL,
+    student_score varchar(50) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
